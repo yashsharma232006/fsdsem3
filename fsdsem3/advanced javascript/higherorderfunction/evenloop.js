2dc9fae9f5f843fb9e7c1771ce1e476569ad999e
yashsharma232006 <yashsharma232006@gmail.com>
2026-09-23 09:52:39 +0530

    changes

--- fsdsem3/advanced javascript/higherorderfunction/evenloop.js
+++ fsdsem3/advanced javascript/higherorderfunction/evenloop.js
@@ -46,15 +46,15 @@
 // console.log("c");
 
 // callstack.js
-function first(){
-    console.log("first");
-    second();
-}
-function second(){
-    console.log("second");
-    third();
-}
-function third(){
-    console.log("third");
-}
-first();
+// function first(){
+//     console.log("first");
+//     second();
+// }
+// function second(){
+//     console.log("second");
+//     third();
+// }
+// function third(){
+//     console.log("third");
+// }
+// first();
